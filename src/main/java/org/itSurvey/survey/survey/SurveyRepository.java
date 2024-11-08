c87ec74fedfb4b350ec922681a0b837d252bf91b
loussalmohammed <hplaptopsimo@gmail.com>
2024-11-08 21:14:50 +0100

    add Survey Controller Defining Handling Http Requests 	Survey Repository Bean interface extending JpaRepository, 	Survey Service interface defining the crud methods signatures

---
+++ src/main/java/org/itSurvey/survey/survey/SurveyRepository.java
@@ -0,0 +1,15 @@
+package org.itSurvey.survey.survey;
+
+import org.itSurvey.survey.utils.exception.ResourceNotFoundException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SurveyRepository extends JpaRepository<Survey, Long> {
+    default Survey findByIdOrThrow(Long id) {
+        return findById(id)
+                .orElseThrow(() -> {
+                    return new ResourceNotFoundException("Survey Not Found Using Specified id!");
+                });
+    }
+}
