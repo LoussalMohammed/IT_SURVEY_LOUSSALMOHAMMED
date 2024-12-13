7fa7d8d5f1dd6ffddb2bcca9b05d799d190f440d
loussalmohammed <hplaptopsimo@gmail.com>
2024-12-13 10:20:09 +0100

    add response answer DTO

--- src/main/java/org/itSurvey/survey/answer/answerDTO/ResponseAnswerDTO.java
+++ src/main/java/org/itSurvey/survey/answer/answerDTO/ResponseAnswerDTO.java
@@ -2,8 +2,10 @@ package org.itSurvey.survey.answer.answerDTO;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import org.itSurvey.survey.question.questionDTO.EmbeddableQuestionDTO;
 
 public record ResponseAnswerDTO(
+    EmbeddableQuestionDTO question,
     @NotNull
     Long id,
     @NotBlank
