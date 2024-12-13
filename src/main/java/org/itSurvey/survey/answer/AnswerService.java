d7d795978e5cc48699d7386e4d2cea9e93953b44
loussalmohammed <hplaptopsimo@gmail.com>
2024-12-13 10:19:42 +0100

    add answer service interface

--- src/main/java/org/itSurvey/survey/answer/AnswerService.java
+++ src/main/java/org/itSurvey/survey/answer/AnswerService.java
@@ -3,12 +3,14 @@ package org.itSurvey.survey.answer;
 import org.itSurvey.survey.answer.answerDTO.RequestAnswerDTO;
 import org.itSurvey.survey.answer.answerDTO.ResponseAnswerDTO;
 import org.itSurvey.survey.answer.answerDTO.UpdateAnswerDTO;
+import org.itSurvey.survey.shared.dto.PageDTO;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface AnswerService {
     ResponseAnswerDTO createAnswer(RequestAnswerDTO requestAnswerDTO);
-    List<ResponseAnswerDTO> getAllAnswers();
+    PageDTO<ResponseAnswerDTO> getAllAnswers(Pageable pageable);
     ResponseAnswerDTO getAnswerById(Long id);
     ResponseAnswerDTO updateAnswerById(UpdateAnswerDTO updateAnswerDTO, Long id);
     void deleteAnswerById(Long id);
